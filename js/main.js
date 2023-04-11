7bd3b60a7ec5afd6ee023acbc94e738c38c441dd
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-04-11 13:53:37 -0300

    add explicacao

--- js/main.js
+++ js/main.js
@@ -5,3 +5,6 @@ robotron.addEventListener("click", dizOi)
 function dizOi(){
     console.log("oi")
 }
+// criamos uma const pra armzenar a busca pelo elemento
+// dizemos que elemento buscado quando clicado deve executar a função
+// criamos a fução que vai ser executada quando clicar
