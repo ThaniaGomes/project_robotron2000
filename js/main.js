d7c1886e776947a42075186c0fa04b4141b46644
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-04-14 13:55:54 -0300

    news

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braço")
 
-robotron.addEventListener("click", (evento) =>{
-    console.log(evento)
+somar.addEventListener("click", (evento) =>{
+    braco.value += 1
 })
 
-function dizOi(nome){
-    console.log("Olá " + nome)
-    console.log("Bem vindo ao Robotron 2000")
-}
-
-dizOi("Nia")
 
 // criamos uma const pra armazenar a busca pelo elemento
 // dizemos que elemento buscado quando clicado deve executar a função
 // criamos a função que vai ser executada quando clicar
-// criamos uma função anonima na linha 3-4 para executar um codigo atrelado a um evento
-//
+// criamos uma função anonima para executar um codigo atrelado a um evento
 
