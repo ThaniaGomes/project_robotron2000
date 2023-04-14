1424cb028840dd297c6d4ead2d17d63da03594da
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-04-14 14:23:24 -0300

    precisa de correcao

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braço")
+const braco = document.querySelector("#braco")
 
-somar.addEventListener("click", (evento) =>{
-    braco.value += 1
+const controle = document.querySelector(".controle-ajuste")
+
+controle.forEach((elemento) => { 
+    elemento.addEventListener("click", () => {
+        console.log(evento.target)
+    })
 })
 
-
+function manipulaDados(operacao) {
+    if(operacao === "-") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
 // criamos uma const pra armazenar a busca pelo elemento
 // dizemos que elemento buscado quando clicado deve executar a função
 // criamos a função que vai ser executada quando clicar
