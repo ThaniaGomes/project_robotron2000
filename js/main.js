23a31905e9a4cace5c7c0ca84ab35bd3b693d4c9
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-04-17 16:45:57 -0300

    comecando novamente

--- js/main.js
+++ js/main.js
@@ -1,23 +1,16 @@
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
+const robotron = document.querySelector("#robotron")
 
-const controle = document.querySelectorAll(".controle-ajuste")
-
-controle.forEach((elemento) => { 
-    elemento.addEventListener("click", (evento) => {
-        console.log(evento)
-        manipulaDados(evento.target)
-    })
+robotron.addEventListener("click", (evento) => {
+    console.log(evento)
 })
 
-function manipulaDados(operacao) {
-    if(operacao === "-") {
-        braco.value = parseInt(braco.value) - 1
-    } else {
-        braco.value = parseInt(braco.value) + 1
-    }
+function dizOi(nome) {
+    console.log('Oi ' + nome)
+    console.log('Bem-vindo ao Robotron 2000')
 }
+
+dizOi("Pedro") 
+
 // criamos uma const pra armazenar a busca pelo elemento
 // dizemos que elemento buscado quando clicado deve executar a função
 // criamos a função que vai ser executada quando clicar
