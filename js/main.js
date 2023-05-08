91c14fb747435cf678813bd129d01661a11ca262
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-05-08 15:38:12 -0300

    tirando fundo robo

--- js/main.js
+++ js/main.js
@@ -58,3 +58,7 @@ function atualizaEstatisticas (peca){
         elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
     })
 }
+
+function trocaImagem(cor){
+    document.querySelector(".robo").src="img/Robotron " + cor + ".png";
+ }
